c2b03d2aee1afc6e2a93c9d50cd2344dfaf65ef2
Ryan Tremblay <ryan.tremblay@microsoft.com>
2021-03-05 17:18:43 -0800

    Babylon type resolution when opening module files directly from module directory
    Gradle error on initial sync

--- Apps/Playground/metro.config.js
+++ Apps/Playground/metro.config.js
@@ -4,17 +4,24 @@
  *
  * @format
  */
+
 const path = require('path');
 const fs = require('fs');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+const appendExclusions = require('metro-config/src/defaults/blacklist');
+
+// Escape function taken from the MDN documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#Escaping
+function escapeRegExp(string) {
+  return string.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
 
 // NOTE: The Metro bundler does not support symlinks (see https://github.com/facebook/metro/issues/1), which NPM uses for local packages.
-//       To work around this, we explicity tell the metro bundler where to find local/linked packages.
+//       To work around this, we supplement the logic to follow symbolic links.
 
 // Create a mapping of package ids to linked directories.
-function getModuleMappings() {
+function processModuleSymLinks() {
   const nodeModulesPath = path.resolve(__dirname, 'node_modules');
   let moduleMappings = {};
+  let moduleExclusions = [];
 
   function findPackageDirs(directory) {
     fs.readdirSync(directory).forEach(item => {
@@ -32,6 +39,15 @@ function getModuleMappings() {
           if (fs.existsSync(packagePath)) {
             const packageId = path.relative(nodeModulesPath, itemPath);
             moduleMappings[packageId] = linkPath;
+
+            const packageInfoData = fs.readFileSync(packagePath);
+            const packageInfo = JSON.parse(packageInfoData);
+
+            // Search for any dev dependencies of the package. They should be excluded from metro so the packages don't get 
+            // imported twice in the bundle.
+            for (let devDependency in packageInfo.devDependencies) {
+              moduleExclusions.push(new RegExp(escapeRegExp(path.join(linkPath, "node_modules", devDependency)) + "\/.*"));
+            }
           }
         }
       } else if (itemStat.isDirectory()) {
@@ -42,10 +58,10 @@ function getModuleMappings() {
 
   findPackageDirs(nodeModulesPath);
 
-  return moduleMappings;
+  return [moduleMappings, moduleExclusions];
 }
 
-const moduleMappings = getModuleMappings();
+const [moduleMappings, moduleExclusions] = processModuleSymLinks();
 console.log("Mapping the following sym linked packages:");
 console.log(moduleMappings);
 
@@ -54,7 +70,7 @@ module.exports = {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: true,
+        inlineRequires: false,
       },
     }),
   },
@@ -62,24 +78,15 @@ module.exports = {
   resolver: {
     // Register an "extra modules proxy" for resolving modules outside of the normal resolution logic.
     extraNodeModules: new Proxy(
-      // Provide the set of known local package mappings.
-      moduleMappings,
-      {
-        // Provide a mapper function, which uses the above mappings for associated package ids,
-        // otherwise fall back to the standard behavior and just look in the node_modules directory.
-        get: (target, name) => name in target ? target[name] : path.join(__dirname, `node_modules/${name}`),
-      },
+        // Provide the set of known local package mappings.
+        moduleMappings,
+        {
+            // Provide a mapper function, which uses the above mappings for associated package ids,
+            // otherwise fall back to the standard behavior and just look in the node_modules directory.
+            get: (target, name) => name in target ? target[name] : path.join(__dirname, `node_modules/${name}`),
+        },
     ),
-    
-    blockList: exclusionList([
-      // Avoid error EBUSY: resource busy or locked, open 'D:\a\1\s\packages\playground\msbuild.ProjectImports.zip' in pipeline
-      /.*\.ProjectImports\.zip/,
-  
-      // This stops "react-native run-windows" from causing the metro server to crash if its already running
-      new RegExp(
-        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
-      ),
-    ]),
+    blacklistRE: appendExclusions(moduleExclusions),
   },
 
   projectRoot: path.resolve(__dirname),
